5d8979fcedc866f30d9c6618d84e73b8fe3c31b7
daxoliveira <dax-oliveira-br@gmail.com>
2020-08-12 13:52:39 -0300

    Manage state with useState Hook

--- src/SearchMovies.js
+++ src/SearchMovies.js
@@ -1,10 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function SearchMovies() {
 
+  const [query, setQuery] = useState('');
+  const [movies, setMovies] = useState([]);
+
   const searchMovies = async (e) => {
     e.preventDefault();
-    console.log("submitting");
 
     const query = "Jurassic Park";
 
@@ -13,7 +15,7 @@ export default function SearchMovies() {
     try {
       const res = await fetch(url);
       const data = await res.json();
-      console.log(data);
+      setMovies(data.results);
     } catch(err) {
       console.error(err)
     }
@@ -22,7 +24,14 @@ export default function SearchMovies() {
   return (
     <form className="form" onSubmit={searchMovies}>
       <label className="label" htmlFor="query">Movie Name</label>
-      <input className="input" type="text"name="query" placeholder="i.e. Jurassic Park"/>
+      <input 
+        className="input" 
+        type="text"
+        name="query" 
+        placeholder="i.e. Jurassic Park"
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+      />
       <button className="button" type="submit">Search</button>
     </form>
   )
